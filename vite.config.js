ea44c7a5952d0def79c97d72e069c41f34cd0b06
Br0u <nightknightbrou@foxmail.com>
2025-04-17 04:02:29 -0400

    添加自定义域名设置，修复CSS语法错误，更新404页面重定向逻辑

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 检查是否为生产环境
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MyBlog/',
+  // 如果是生产环境且自定义域名存在，则base为'/'，否则为'/MyBlog/'
+  base: isProd ? '/' : '/MyBlog/',
 })
